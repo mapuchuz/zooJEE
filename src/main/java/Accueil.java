73acbf3cc01becf0273d78690ae94a841da988db
Vincent Demolombe <vincent.demolombe@sfr.fr>
2016-03-16 15:43:23 +0100

    page acceuil (très laide) 3 choix: enclos,animaux,visites

---
+++ src/main/java/Accueil.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Accueil {
+
+//	static private List<Enclos>enclos= 	new ArrayList<>();
+
+	public String enclos() {
+		return null;
+	}
+	public String animaux() {
+		return null;
+	}
+	public String visites() {
+		return null;
+	}
+	
+	
+}
